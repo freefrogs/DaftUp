01feec02f9c777a73790136b1293be24974f1d1f
Justyna Biernacka <justyna.biernacka.1986@gmail.com>
2019-05-12 22:50:07 +0200

    Add calculation of final price - part of task 3

---
+++ app/controllers/carts/totals_controller.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+module Carts
+  class TotalsController < ApplicationController
+
+    def show
+      render json: summary.to_json
+    end
+
+    private
+  
+    def cart
+      Cart.find(1)
+    end
+
+    def products
+      Product.all
+    end
+
+    def product_list
+      cart.items.all
+    end
+
+    def discounts
+      cart.discounts.all
+    end
+
+    def regular_price
+      product_list.sum { |e| e.quantity * products.find(e.product_id).price }
+    end
+
+    def final_price
+      ids = product_list.map { |e| Array.new(e.quantity,e.product_id) }.sum
+
+      total_price = discounts.map { |e|
+
+        if e.kind == "set"
+
+          ids_list = ids
+          common_part = (e.product_ids & ids_list).flat_map { |n| [n]*[e.product_ids.count(n), ids_list.count(n)].min }
+
+          if e.product_ids.length == common_part.length
+            e.product_ids.map { |n| ids_list.delete_at(ids_list.index(n)) }
+            ids = ids_list
+            e.price
+          end
+        
+        else
+          count = e.count + 1
+          extra_ids = e.product_ids.map { |n| Array.new(count,n) }
+
+          extra_ids.map { |arr|
+
+            ids_list = ids
+            common_part = ids_list.each_with_object(arr.dup).map{|v,t| v if (l = t.index v) && t.slice!(l) }.compact
+
+            if arr.length == common_part.length
+              arr.map { |n| ids_list.delete_at(ids_list.index(n)) }
+              ids = ids_list
+              extra_price = products.find(arr.uniq)[0][:price] * e.count
+            end
+          }
+        end
+      }.compact.flatten.compact.sum
+
+      regular_products_price = ids.sum { |e| products.find(e).price }
+      total_price += regular_products_price.round(2)
+    end
+
+    def summary
+      {
+        "products in cart": product_list,
+        "your dicounts": discounts,
+        "regular price": regular_price,
+        "final price": final_price
+      }      
+    end
+
+  end
+end
+
+# methods for common part
+#1) common_part = ids.each_with_object(sets_ids.dup).map{|v,t| v if (l = t.index v) && t.slice!(l) }.compact
+#2) (array1 & array2).flat_map { |n| [n]*[array1.count(n), array2.count(n)].min }
+#3) short_arr.map { |e| long_arr.delete_at(long_arr.index(e)) } 
+#   when we do "return long_arr" we will get long_arr minus short_arr (with doubles)
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,10 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_084849) do
+ActiveRecord::Schema.define(version: 2019_05_07_173313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.string "kind", null: false
+    t.string "name", null: false
+    t.integer "product_ids", default: [], null: false, array: true
+    t.float "price", default: 0.0
+    t.integer "count", default: 0
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_discounts_on_cart_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "quantity", default: 1
+    t.bigint "product_id"
+    t.bigint "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_items_on_cart_id"
+    t.index ["product_id"], name: "index_items_on_product_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,4 +51,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_084849) do
     t.index ["name"], name: "index_products_on_name", unique: true
   end
 
+  add_foreign_key "discounts", "carts"
+  add_foreign_key "items", "carts"
+  add_foreign_key "items", "products"
 end
